d8875f2f4fba31b71a449fd0e58e168c216b0565
KYD <kydkang@gmail.com>
2018-11-20 12:17:37 +0900

    9a

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -7,5 +7,5 @@ def index(request):
 
 def today_is(request):
     now = datetime.datetime.now()
-    html = "<html><body>Current date and time: {0}</body></html>".format(now)
-    return HttpResponse(html)
+    return render(request, 'blog/datetime.html', {'now':now})
+    
